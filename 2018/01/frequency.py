ae583f39a22cfc23717c5ba257da28847c548f34
Stacy Brock <kalrnux@gmail.com>
2018-11-30 22:03:30 -0800

    Add solution to 2018 day 1

---
+++ 2018/01/frequency.py
@@ -0,0 +1,34 @@
+# solution to Advent of Code 2018, day 1 part one and two
+# https://adventofcode.com/2018/day/1
+#
+# assumes puzzle input is in a file called input.txt
+
+def main():
+    # get list of freq changes from input
+    changes = []
+    with open('input.txt') as inputfile:
+        changes = list(map(int, inputfile.readlines()))
+
+    # solve day 1 puzzle
+    current_freq = 0
+    for change in changes:
+        current_freq += change
+    print(f"Resulting frequency: {current_freq}")
+
+    # set up vars for day 2 puzzle
+    freqs_seen = []
+    freqs_seen.append(0)
+    current_freq = 0
+
+    # solve day 2 puzzle
+    while True:
+        for change in changes:
+            current_freq += change
+            if current_freq in freqs_seen:
+                print(f"Found duplicate frequency: {current_freq}")
+                return
+            freqs_seen.append(current_freq)
+
+
+if __name__ == '__main__':
+    main()
