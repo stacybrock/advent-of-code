991e2f31c28f709655065966d6f82de789293985
Stacy Brock <kalrnux@gmail.com>
2018-12-09 22:19:25 -0800

    Improve 2018/02 solution

--- 2018/02/inventory.py
+++ 2018/02/inventory.py
@@ -9,30 +9,20 @@ def main():
     box_ids = list(line.strip() for line in fileinput.input())
 
     # solve part one
-    two_counts = three_counts = 0
+    two_counts = 0
+    three_counts = 0
     for box_id in box_ids:
-        uniq_chars = set(box_id)
-
-        for char in uniq_chars:
-            if box_id.count(char) == 2:
-                two_counts += 1
-                break
-
-        for char in uniq_chars:
-            if box_id.count(char) == 3:
-                three_counts += 1
-                break
-
+        two_counts += sum(set([1 for c in set(box_id) if box_id.count(c) == 2]))
+        three_counts += sum(set([1 for c in set(box_id) if box_id.count(c) == 3]))
     print(f"Checksum: {two_counts * three_counts}")
 
     # solve part two
     seen_ids = []
     for box_id in box_ids:
         for seen_id in seen_ids:
-            differences = sum(1 for a, b in zip(box_id, seen_id) if a != b)
-            if differences == 1:
-                common_letters = ''.join(list(a for a, b in zip(box_id, seen_id) if a == b))
-                print(f"Common letters between the correct box IDs: {common_letters}")
+            common_letters = [a for a,b in zip(box_id, seen_id) if a == b]
+            if len(common_letters) == len(seen_id)-1:
+                print(f"Common letters between the correct box IDs: {''.join(common_letters)}")
         seen_ids.append(box_id)
 
 
